117ffbe61d2f10fb5ef34ee632d326e7c561cdf5
ShihaHib1 <137032284+ShihaHib1@users.noreply.github.com>
2023-06-20 11:42:47 -0600

    Update Estilista.java

--- src/java/modelos/Estilista.java
+++ src/java/modelos/Estilista.java
@@ -7,22 +7,33 @@ package modelos;
 
 
 /**
- *
+ * Esta clase representa a un estilista de la barberia
  * @author alber
  */
 public class Estilista {
-    
     private String email;
     private String nombre;
     private String apellido;
     private String contraseña;
     private String estado;
     private String horario;
-    
 
+    /**
+     * Crea una instancia de Estilista sin inicializar los atributos.
+     */
     public Estilista() {
     }
 
+    /**
+     * Crea una instancia de Estilista con los atributos especificados.
+     * 
+     * @param email      El correo electrónico del estilista.
+     * @param nombre     El nombre del estilista.
+     * @param apellido   El apellido del estilista.
+     * @param contraseña La contraseña del estilista.
+     * @param estado     El estado del estilista (activo, inactivo, etc.).
+     * @param horario    El horario de trabajo del estilista.
+     */
     public Estilista(String email, String nombre, String apellido, String contraseña, String estado, String horario) {
         this.email = email;
         this.nombre = nombre;
@@ -32,57 +43,111 @@ public class Estilista {
         this.horario = horario;
     }
 
+    /**
+     * Obtiene el correo electrónico del estilista.
+     * 
+     * @return El correo electrónico del estilista.
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Establece el correo electrónico del estilista.
+     * 
+     * @param email El correo electrónico del estilista.
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Obtiene el apellido del estilista.
+     * 
+     * @return El apellido del estilista.
+     */
     public String getApellido() {
         return apellido;
     }
 
+    /**
+     * Establece el apellido del estilista.
+     * 
+     * @param apellido El apellido del estilista.
+     */
     public void setApellido(String apellido) {
         this.apellido = apellido;
     }
 
+    /**
+     * Obtiene la contraseña del estilista.
+     * 
+     * @return La contraseña del estilista.
+     */
     public String getContraseña() {
         return contraseña;
     }
 
+    /**
+     * Establece la contraseña del estilista.
+     * 
+     * @param contraseña La contraseña del estilista.
+     */
     public void setContraseña(String contraseña) {
         this.contraseña = contraseña;
     }
 
+    /**
+     * Obtiene el estado del estilista.
+     * 
+     * @return El estado del estilista.
+     */
     public String getEstado() {
         return estado;
     }
 
+    /**
+     * Establece el estado del estilista.
+     * 
+     * @param estado El estado del estilista.
+     */
     public void setEstado(String estado) {
         this.estado = estado;
     }
 
+    /**
+     * Obtiene el horario de trabajo del estilista.
+     * 
+     * @return El horario de trabajo del estilista.
+     */
     public String getHorario() {
         return horario;
     }
 
+    /**
+     * Establece el horario de trabajo del estilista.
+     * 
+     * @param horario El horario de trabajo del estilista.
+     */
     public void setHorario(String horario) {
         this.horario = horario;
     }
 
-   
-    
+    /**
+     * Obtiene el nombre del estilista.
+     * 
+     * @return El nombre del estilista.
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Establece el nombre del estilista.
+     * 
+     * @param nombre El nombre del estilista.
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
-    
-    
-    
 }
